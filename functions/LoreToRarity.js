8af41329d6640cf6b6aa1ff79939057dbae2fa8d
meiguess <tobiasyoutobs@gmail.com>
2022-07-09 19:05:54 +0200

    Added LoreToRarity Function

---
+++ functions/LoreToRarity.js
@@ -0,0 +1,30 @@
+module.exports = (lore) => {
+  const lastText = lore[lore.length - 1];
+  if (lastText.includes("COMMON")) {
+    return "COMMON";
+  }
+  if (lastText.includes("UNCOMMON")) {
+    return "UNCOMMON";
+  }
+  if (lastText.includes("RARE")) {
+    return "RARE";
+  }
+  if (lastText.includes("EPIC")) {
+    return "EPIC";
+  }
+  if (lastText.includes("LEGENDARY")) {
+    return "LEGENDARY";
+  }
+  if (lastText.includes("MYTHIC")) {
+    return "MYTHIC";
+  }
+  if (lastText.includes("SUPREME")) {
+    return "SUPREME";
+  }
+  if (lastText.includes("SPECIAL")) {
+    return "SPECIAL";
+  }
+  if (lastText.includes("VERY") && lastText.includes("SPECIAL")) {
+    return "VERY SPECIAL";
+  }
+}
