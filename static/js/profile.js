72769004c29633856ca85adccccf67fabdb81780
meiguess <tobiasyoutobs@gmail.com>
2022-07-18 18:48:43 +0200

    Added itemName Variable

--- static/js/profile.js
+++ static/js/profile.js
@@ -13,6 +13,7 @@ allPieces.forEach((e) => {
     e.addEventListener("mouseover", (event) => {
       let pos = event.target.getClientRects()[0];
       let rarity = e.classList[1].replace("piece-", "").replace("-bg", "");
+      let itemName = e.classList[e.classList.length - 1].split("_").join(" ");
       document.querySelector(".item-name").classList.add("piece-" + rarity + "-bg");
       document.querySelector("#stats_content").style.top = (pos.top - 250) + "px";
       document.querySelector("#stats_content").style.left = (pos.left - 400) + "px";
