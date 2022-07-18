58e04f75e8f9386b0de01d76efc1fbebee9e7581
meiguess <tobiasyoutobs@gmail.com>
2022-07-18 18:28:31 +0200

    Background of Name

--- static/js/profile.js
+++ static/js/profile.js
@@ -13,6 +13,7 @@ allPieces.forEach((e) => {
     e.addEventListener("mouseover", (event) => {
       let pos = event.target.getClientRects()[0];
       let rarity = e.classList[1].replace("piece-", "").replace("-bg", "");
+      document.querySelector(".item-name").classList.add("piece-" + rarity + "-bg");
       document.querySelector("#stats_content").style.top = (pos.top - 250) + "px";
       document.querySelector("#stats_content").style.left = (pos.left - 400) + "px";
       document.querySelector("#stats_content").style.display = "block";
@@ -22,6 +23,15 @@ allPieces.forEach((e) => {
 
 allPieces.forEach((e) => {
   e.addEventListener("mouseout", () => {
+    document.querySelector(".item-name").classList.remove("piece-COMMON-bg");
+    document.querySelector(".item-name").classList.remove("piece-UNCOMMON-bg");
+    document.querySelector(".item-name").classList.remove("piece-RARE-bg");
+    document.querySelector(".item-name").classList.remove("piece-EPIC-bg");
+    document.querySelector(".item-name").classList.remove("piece-LEGENDARY-bg");
+    document.querySelector(".item-name").classList.remove("piece-MYTHIC-bg");
+    document.querySelector(".item-name").classList.remove("piece-SUPREME-bg");
+    document.querySelector(".item-name").classList.remove("piece-SPECIAL-bg");
+    document.querySelector(".item-name").classList.remove("piece-VERY_SPECIAL-bg");
     document.querySelector("#stats_content").style.display = "none";
   });
 });
