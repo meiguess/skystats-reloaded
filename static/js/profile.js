00538e5d217cb25ba08254ac8831d54116fd8937
meiguess <tobiasyoutobs@gmail.com>
2022-07-18 18:21:16 +0200

    Get Rarity

--- static/js/profile.js
+++ static/js/profile.js
@@ -12,7 +12,7 @@ allPieces.forEach((e) => {
   if (!e.classList.contains("piece-NONE-bg")) {
     e.addEventListener("mouseover", (event) => {
       let pos = event.target.getClientRects()[0];
-      console.log("hover");
+      let rarity = e.classList[1].replace("piece-", "").replace("-bg", "");
       document.querySelector("#stats_content").style.top = (pos.top - 250) + "px";
       document.querySelector("#stats_content").style.left = (pos.left - 400) + "px";
       document.querySelector("#stats_content").style.display = "block";
