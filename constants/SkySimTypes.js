f49e92db69b4316691ead8a5a7ce79d9f12ef19e
meiguess <tobiasyoutobs@gmail.com>
2022-07-18 13:09:09 +0200

    Added all /ib swords

--- constants/SkySimTypes.js
+++ constants/SkySimTypes.js
@@ -1,4 +1,14 @@
 module.exports = {
   'EMERALD_BLADE': '../Resources/custom/items/melee/emerald_blade.png',
-  'EDIBLE_MACE': '../Resources/custom/items/melee/edible_mace.png'
+  'EDIBLE_MACE': '../Resources/custom/items/melee/edible_mace.png',
+  'ASPECT_OF_THE_DRAGONS': '../Resources/custom/items/melee/aspect_dragon.png',
+  'ASPECT_OF_THE_END': '../Resources/custom/items/melee/aspect_of_the_end.png',
+  'FROZEN_SCYTHE': '../Resources/custom/items/melee/frozen_scythe.png',
+  'LEAPING_SWORD': '../Resources/custom/items/melee/leaping_sword.png',
+  'ZOMBIE_SWORD_T2': '../Resources/custom/items/melee/ornatezombiesword.png',
+  'ZOMBIE_SWORD_T3': '../Resources/custom/items/melee/floridzombiesword.png',
+  'POOCH_SWORD': '../Resources/custom/items/melee/pooch_sword.png',
+  'PRISMARINE_BLADE': '../Resources/custom/items/melee/prismarine_blade.png',
+  'REAPER_FALCHION': '../Resources/custom/items/melee/reaper_falchion.png',
+  'ROGUE_SWORD': '../Resources/custom/items/melee/rogue_sword.png',
 }
