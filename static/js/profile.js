5f41b178aff98351a5c76598c5bf2729c009b481
meiguess <tobiasyoutobs@gmail.com>
2022-07-18 18:11:14 +0200

    Stat Content wont show if there's no item

--- static/js/profile.js
+++ static/js/profile.js
@@ -9,13 +9,15 @@ document.querySelector("#favorized").addEventListener("click", () => {
 }); */
 const allPieces = document.querySelectorAll(".piece");
 allPieces.forEach((e) => {
-  e.addEventListener("mouseover", (event) => {
-    let pos = event.target.getClientRects()[0];
-    console.log("hover");
-    document.querySelector("#stats_content").style.top = (pos.top - 250) + "px";
-    document.querySelector("#stats_content").style.left = (pos.left - 400) + "px";
-    document.querySelector("#stats_content").style.display = "block";
-  });
+  if (!e.classList.contains("piece-NONE-bg")) {
+    e.addEventListener("mouseover", (event) => {
+      let pos = event.target.getClientRects()[0];
+      console.log("hover");
+      document.querySelector("#stats_content").style.top = (pos.top - 250) + "px";
+      document.querySelector("#stats_content").style.left = (pos.left - 400) + "px";
+      document.querySelector("#stats_content").style.display = "block";
+    });
+  }
 });
 
 allPieces.forEach((e) => {
